e237beaa0314e89047a4cf6ef37bc02bb1ebe690
Rodzers <rodzers.usackis@gmail.com>
2023-04-24 15:32:46 +0200

    Implemented Quality Gate Status In README.md

--- nextjs-frontend/src/pages/_document.tsx
+++ nextjs-frontend/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head ><title>Document Head</title></Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
