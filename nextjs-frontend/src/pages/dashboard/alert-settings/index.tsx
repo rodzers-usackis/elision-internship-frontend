94ccb4dd1c9235a5785c52c144b1b97db22d9e9f
rodzers.usackis <rodzers.usackis@student.kdg.be>
2023-06-02 10:21:28 +0200

    Misc. fixes

--- nextjs-frontend/src/pages/dashboard/alert-settings/index.tsx
+++ nextjs-frontend/src/pages/dashboard/alert-settings/index.tsx
@@ -12,17 +12,24 @@ export default function AlertsSettings() {
 
     const {isAlertSettingsError, alertSettings, isAlertSettingsLoading} = useAlertSettings();
 
-    function PageComponent(){
-        return(
-            <Grid item className={styles.lineChart}>
-                {isAlertSettingsLoading && <CircularProgress/>}
-                {isAlertSettingsError && <Alert severity="error">Error loading alert settings</Alert>}
-                {!isAlertSettingsLoading && !isAlertSettingsError && alertSettings &&
-                    <AlertSettingsForm alertSettings={alertSettings}/>}
-
+    function PageComponent() {
+        return (
+            <Grid item className={styles.contentWrapper}>
+                {isAlertSettingsLoading ? (
+                    <CircularProgress/>
+                ) : isAlertSettingsError ? (
+                    <Alert severity="error">Error loading alerts</Alert>
+                ) : (
+                    !isAlertSettingsLoading && !isAlertSettingsError && alertSettings && (
+                        <Grid item className={styles.lineChart}>
+                            <AlertSettingsForm alertSettings={alertSettings}/>
+                        </Grid>
+                    )
+                )}
             </Grid>
-        )
+        );
     }
+
 
     return (
         <DashboardDrawerPageTemplate
