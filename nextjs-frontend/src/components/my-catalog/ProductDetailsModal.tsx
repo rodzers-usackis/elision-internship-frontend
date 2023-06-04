9cedc6effcec090059074697b3d36e529bd880a3
Dominik Mika <dominik.mika@student.kdg.be>
2023-06-04 11:32:14 +0200

    Add product details modal to my catalog page

--- nextjs-frontend/src/components/my-catalog/ProductDetailsModal.tsx
+++ nextjs-frontend/src/components/my-catalog/ProductDetailsModal.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
 
 interface ProductDetailsModalProps {
     open: boolean;
@@ -18,26 +21,65 @@ export default function ProductDetailsModal({product, open, onClose}: ProductDet
         return (<></>)
     }
 
-    return (<Modal
+    return (
+        <Modal
             sx={{
-                overflow: "scroll",
+                overflow: "auto",
                 padding: "1rem",
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
             }}
-            open={open} onClose={onClose}>
-            <Card sx={{width: 'fit-content'}}>
-                <CardContent>
+            open={open}
+            onClose={onClose}
+        >
+            <Card
+                sx={{
+                    width: "fit-content",
+                    padding: "1rem",
+                    position: "relative",
+                    maxHeight: "90vh",
+                    overflowY: "auto"
+                }}
+            >
+                <CardContent sx={{ position: "relative", zIndex: 1 }}>
                     <Typography gutterBottom variant="h5" component="div">
                         {product.name}
+                    </Typography>
+                </CardContent>
+
+                <IconButton
+                    aria-label="Close"
+                    sx={{
+                        position: "absolute",
+                        top: "-0.5rem",
+                        right: "-0.5rem",
+                        zIndex: 2,
+                        padding: '1.5rem'
+                    }}
+                    onClick={onClose}
+                >
+                    <CloseIcon />
+                </IconButton>
+
+                <CardMedia component="img" height="140" image={product.imageUrl} alt={product.name} />
+
+                <CardContent sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+                    <Typography variant="body2" color="text.secondary">
+                        {product.ean} | {product.manufacturerCode}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         {product.description}
                     </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {product.category}
+                    </Typography>
                 </CardContent>
             </Card>
         </Modal>
-    )
+);
+
+
+
 
 }
