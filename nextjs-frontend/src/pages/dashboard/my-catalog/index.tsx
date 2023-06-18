a88d67808137c58afac799cdb19e68e309c5b160
Dominik Mika <dominik.mika@student.kdg.be>
2023-06-18 16:16:39 +0200

    Small fixes

--- nextjs-frontend/src/pages/dashboard/my-catalog/index.tsx
+++ nextjs-frontend/src/pages/dashboard/my-catalog/index.tsx
@@ -164,7 +164,7 @@ export default function MyCatalog() {
         if (searchInputRef.current && document.activeElement !== searchInputRef.current) {
             searchInputRef.current.focus();
         }
-    }, [searchText]);
+    }, [searchText, catalogTableData]);
 
     function ActionShelf() {
         return (
