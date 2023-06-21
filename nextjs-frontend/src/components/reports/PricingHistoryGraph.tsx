957714a8395714075058364d1372b5597f7f5dfe
Dominik Mika <dominik.mika@student.kdg.be>
2023-06-21 17:18:42 +0200

    Fix graph

--- nextjs-frontend/src/components/reports/PricingHistoryGraph.tsx
+++ nextjs-frontend/src/components/reports/PricingHistoryGraph.tsx
@@ -47,13 +47,18 @@ const options = {
 };
 
 const colors = [
+
     {
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)'
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)'
     },
     {
         backgroundColor: 'rgba(255, 159, 64, 0.2)',
         borderColor: 'rgb(255, 159, 64)'
+    },
+    {
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)'
     },
     {
         backgroundColor: 'rgba(255, 205, 86, 0.2)',
@@ -64,8 +69,8 @@ const colors = [
         borderColor: 'rgb(75, 192, 192)'
     },
     {
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgb(54, 162, 235)'
+        backgroundColor: 'rgba(97,239,64,0.2)',
+        borderColor: 'rgb(109,255,99)'
     },
     {
         backgroundColor: 'rgba(153, 102, 255, 0.2)',
@@ -74,43 +79,50 @@ const colors = [
     {
         backgroundColor: 'rgba(201, 203, 207, 0.2)',
         borderColor: 'rgb(201, 203, 207)'
-    },
-    {
-        backgroundColor: 'rgba(97,239,64,0.2)',
-        borderColor: 'rgb(109,255,99)'
     }
 ]
 
 export function PricingHistoryGraph({data}: { data?: ProductPriceHistorian }) {
-    const timestamps = data?.data[0].timestampAmounts.map((item) => item.timestamp);
-    const amounts = data?.data[0].timestampAmounts.map((item) => item.amount);
-
-    const chartData = {
-        labels: timestamps?.map((timestamp) => dayjs(timestamp).format('YYYY-MM-DD')),
-        datasets: [{
-            label: data?.product.name,
-            data: amounts,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
-            backgroundColor: 'rgba(53, 162, 235, 0.2)',
-        }],
+    let timestamps: string[] = [];
+    let chartData: ChartData<"line", (number | null)[], string> = {
+        labels: [],
+        datasets: [],
     };
 
-    const chartData2: ChartData<"line", number[], string> = {
-        labels: timestamps?.map((timestamp) => dayjs(timestamp).format('YYYY-MM-DD')),
-        datasets: data?.data.map((item, index) => {
-            return {
+    if (data) {
+        // Collect all unique timestamps and round them to the day only
+        const uniqueTimestampsSet = new Set<string>();
+        data.data.forEach((item) => {
+            item.timestampAmounts.forEach((timestampAmount) => {
+                const roundedTimestamp = dayjs(timestampAmount.timestamp).startOf('day').format('YYYY-MM-DD');
+                uniqueTimestampsSet.add(roundedTimestamp);
+            });
+        });
+
+        // Convert the Set to an array and sort the timestamps in ascending order
+        timestamps = Array.from(uniqueTimestampsSet).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+        // Prepare the chart data
+        chartData.labels = timestamps;
+        chartData.datasets = data.data.map((item, index) => {
+            const dataset = {
                 label: item.retailerCompanyDto.name,
-                data: item.timestampAmounts.map((item) => item.amount),
+                data: Array(timestamps.length).fill(null) as (number | null)[],
                 borderColor: colors[index].borderColor,
                 tension: 0.1,
                 backgroundColor: colors[index].backgroundColor,
-            } as ChartDataset<"line", number[]>;
-        }) || [],
-    };
+            };
+            item.timestampAmounts.forEach((timestampAmount) => {
+                const roundedTimestamp = dayjs(timestampAmount.timestamp).startOf('day').format('YYYY-MM-DD');
+                const timestampIndex = timestamps.indexOf(roundedTimestamp);
+                if (timestampIndex !== -1) {
+                    dataset.data[timestampIndex] = timestampAmount.amount;
+                }
+            });
+            return dataset;
+        });
+    }
 
 
-
-    console.log(data)
-    return <Line options={options} data={chartData2}/>
+    return <Line options={options} data={chartData}/>
 }
